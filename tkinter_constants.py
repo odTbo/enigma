7e1849eb013987a3d5706e2f3c610543023e811f
Vladimir <htcvlado@gmail.com>
2021-08-11 03:33:10 +0200

    TODO DATABASE BASE OF UI

--- tkinter_constants.py
+++ tkinter_constants.py
@@ -1,6 +1,8 @@
 FONT = ("Calibri", 10, "normal")
 
 # ADD/EDIT PERSON FORM
+# ----------------(left, right)
 FORM_ENTRY_PADX = (30, 0)
+# ----------------(top, bottom)
 FORM_ENTRY_PADY = (0, 10)
 FORM_ENTRY_WDTH = 30
