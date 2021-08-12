d187f693889843892a92067d2aef4a6adf903b42
Vladimir <htcvlado@gmail.com>
2021-08-13 01:44:44 +0200

    WORKING INSERT WORKING EDIT BIRTHDAY CHECKER BASE TODO DELETE ENTRY

--- birthday_check.py
+++ birthday_check.py
@@ -9,13 +9,18 @@ data = execute_read_query(connection, select_all_query)
 today = datetime.today()
 date = today.strftime("%d/%m")
 
-no_birthday = True
+no_tasks = True
 for person_data in data:
     full_name = f"{person_data[1]} {person_data[2]}"
     birthday = person_data[4]
+    nameday = person_data[5]
     if birthday == date:
         print(f"Today is {date} and {full_name} has his birthday.")
-        no_birthday = False
+        no_tasks = False
+    if nameday == date:
+        print(f"Today is {date} and {full_name} has his nameday.")
+        no_tasks = False
 
-if no_birthday:
+
+if no_tasks:
     print("No friend has birthday today.")
