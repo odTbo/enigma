8ca4207a928d44943976fd51f63b8fde332fb8f3
Vladimir <htcvlado@gmail.com>
2021-08-14 00:20:15 +0200

    WORKING INSERT WORKING EDIT WORKING BIRTHDAY CHECKER IG MANAGER BASE TODO DELETE ENTRY

--- birthday_check.py
+++ birthday_check.py
@@ -6,47 +6,64 @@ import os
 
 load_dotenv()
 
-# Establish DB connection
-connection = create_connection()
-
-# Fetch Data
-data = execute_read_query(connection, select_all_query)
-
 today = datetime.today()
 date = today.strftime("%d/%m")
 
-messages = []
-get_notified = False
-for person_data in data:
-    full_name = f"{person_data[1]} {person_data[2]}"
-    birthday = person_data[4]
-    nameday = person_data[5]
-    if birthday == date:
-        birthday_message = f"Today is {date} and {full_name} has his birthday.\n"
-        messages.append(birthday_message)
-        get_notified = True
-    if nameday == date:
-        nameday_message = f"Today is {date} and {full_name} has his nameday.\n"
-        messages.append(nameday_message)
-        get_notified = True
 
-sender_email = os.getenv("SENDER_EMAIL")
-email_pass = os.getenv("PASSWORD")
-to_email = os.getenv("TO_EMAIL")
-mail_content = f"Subject: Birthday/Nameday {date}\n\n"
+class BirthdayChecker:
+    def __init__(self):
+        self.connection = create_connection() # Establish DB connection
+        self.data = execute_read_query(self.connection, select_all_query) # Fetch Data
+        self.sender_email = os.getenv("SENDER_EMAIL")
+        self.email_pass = os.getenv("PASSWORD")
+        self.to_email = os.getenv("TO_EMAIL")
+        self.mail_content = ""
+        self.messages = []
+        self.get_notified = False
 
+    def run(self):
+        self.check_birthday()
+        if self.get_notified:
+            self.send_notification_email()
+        else:
+            print("[Birthday Checker] No birthday/nameday matches found for today.")
 
-if get_notified:
-    for message in messages:
-        mail_content += message
+    def check_birthday(self):
+
+        for person_data in self.data:
+            full_name = f"{person_data[1]} {person_data[2]}"
+            birthday = person_data[4]
+            nameday = person_data[5]
+
+            if birthday == date:
+                birthday_message = f"Today is {date} and {full_name} has his birthday.\n"
+                self.messages.append(birthday_message)
+                self.get_notified = True
+
+            if nameday == date:
+                nameday_message = f"Today is {date} and {full_name} has his nameday.\n"
+                self.messages.append(nameday_message)
+                self.get_notified = True
+
+    def send_notification_email(self):
+
+        self.mail_content = f"Subject: Birthday/Nameday {date}\n\n"
+        for message in self.messages:
+            self.mail_content += message
+
         try:
-            print("Sending...")
-            with smtplib.SMTP_SSL("smtp.gmail.com") as connection:
-                connection.login(user=sender_email, password=email_pass)
-                connection.sendmail(
-                    from_addr=sender_email,
-                    to_addrs=to_email,
-                    msg=mail_content
+            print("[Birthday Checker] Sending email...")
+            with smtplib.SMTP_SSL("smtp.gmail.com") as server:
+                server.login(user=self.sender_email, password=self.email_pass)
+                server.sendmail(
+                    from_addr=self.sender_email,
+                    to_addrs=self.to_email,
+                    msg=self.mail_content
                 )
         finally:
-            print("Done.")
+            print("[Birthday Checker] Done.")
+
+
+if __name__ == "__main__":
+    birthday_checker = BirthdayChecker()
+    birthday_checker.run()
